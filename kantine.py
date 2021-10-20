ca69da408ccd9916505ea16b26149cd1ac511bb8
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-10-20 08:42:31 +0200

    flytt all datahenting inn i funksjonen, ikke bare litt av den...

--- kantine.py
+++ kantine.py
@@ -6,18 +6,8 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.sit.no/gjovik/mat'
-user_agent =  {'User-Agent': 'Mozilla/5.0(X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0'}
-page = requests.get(URL, headers=user_agent)
-soup = BeautifulSoup(page.content, "html.parser")
-menu = soup.find_all("li", class_="dishes__day")
-
-remove = 'Plukk og mix'
-
 menu_dict = {}
-
 today = datetime.datetime.today().weekday()
-
 week_translation = {
         'mandag': 0,
         'tirsdag': 1,
@@ -27,6 +17,13 @@ week_translation = {
         }
 
 def fetch_menu():
+    URL = 'https://www.sit.no/gjovik/mat'
+    user_agent =  {'User-Agent': 'Mozilla/5.0(X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0'}
+    page = requests.get(URL, headers=user_agent)
+    soup = BeautifulSoup(page.content, "html.parser")
+    menu = soup.find_all("li", class_="dishes__day")
+    remove = 'Plukk og mix'
+
     for element in menu:
         day = element.find("h4").text.strip().lower()
         dishes = element.find_all("li", class_="dishes__dishes__dish")
@@ -34,6 +31,8 @@ def fetch_menu():
             if not re.findall(remove, dish.text):
                 menu_dict[day] = dish.contents[0].strip().replace(', fisk og vegetar', '')
 
+## Program starts here ##
+#########################
 if len(sys.argv) < 2:
     fetch_menu()
     for day, food in menu_dict.items():
