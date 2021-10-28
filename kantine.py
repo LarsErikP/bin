f9a1fa60abdccbf3d389058bb467c75cdea6e529
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-10-28 09:59:04 +0200

    Fiks ubrukelig kommabruk

--- kantine.py
+++ kantine.py
@@ -33,7 +33,7 @@ def fetch_menu():
         dishes = element.find_all("li", class_="dishes__dishes__dish")
         for dish in dishes:
             if not re.findall(remove, dish.text):
-                menu_dict[day] = dish.contents[0].strip().replace(', fisk og vegetar', '')
+                menu_dict[day] = re.sub(r',\s*fisk og vegetar', '', dish.contents[0].strip())
 
 ## Program starts here ##
 #########################
